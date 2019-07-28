1bb03f85a2340b99f5f971a9ea427aca4ad7f236
Eric Silva <ericdss@hotmail.com>
2019-07-28 20:11:14 -0300

    CRUD Scaffolding

---
+++ SalesWebMVC/Data/SWContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMVC.Models
+{
+    public class SWContext : DbContext
+    {
+        public SWContext (DbContextOptions<SWContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+    }
+}
