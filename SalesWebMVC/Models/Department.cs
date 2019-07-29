90e3dd0c1541e90bb8e66b302d181bc849aeb8a1
Eric Silva <ericdss@hotmail.com>
2019-07-29 16:59:08 -0300

    Segunda migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,5 +9,26 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        { }
+
+        public Department(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+
+            return Sellers.Sum(x => x.TotalSales(initial, final));
+
+        }
     }
 }
