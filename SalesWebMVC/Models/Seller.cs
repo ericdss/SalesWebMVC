c031a415267e61a6723aac9cbc641ea0c5f9f135
Eric Silva <ericdss@hotmail.com>
2019-08-03 18:07:17 -0300

    formatação de textos e dados

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace SalesWebMVC.Models
 {
     public class Seller
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome")] //permite personalizar o nome de exibição
         public string Name { get; set; }
+
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
+
+        [Display(Name = "Data de Nascimento"), DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Salário Base"), DisplayFormat(DataFormatString ="{0:f2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
